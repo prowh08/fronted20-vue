257bcc78aae09fc47b5047e6938a8826685b529e
yoon <pro_wh08@naver.com>
2020-05-04 11:00:23 +0900

    시험5

--- hw/exam/mid4.js
+++ hw/exam/mid4.js
@@ -1,12 +1,22 @@
-function sum() {
-    
+let sum = function(...a) {
+    let result=0;
+    for(let i=0;i<1;++i){
+        result+=a[0];
+        console.log(result)
+    }
+    a.reduce
+   
+    return result;
 }
 
-function testsum() {
+function testsum(f) {
     let arr=[5];
    for(let i=0;i<5;++i)
    arr[i]=Math.floor(Math.random()*11);
    console.log(arr);
+
+   
 }
+
 for(let i=0;i<5;++i)
-    console.log(testsum(sum()))
+    testsum(sum)
